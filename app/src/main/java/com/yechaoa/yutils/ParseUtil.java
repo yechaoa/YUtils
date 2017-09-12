4b2c4813cd1769c1b90167a82657260d7759a85d
bige-ye <819423475@qq.com>
2017-09-12 12:01:51 +0800

    ParseUtil

---
+++ app/src/main/java/com/yechaoa/yutils/ParseUtil.java
@@ -0,0 +1,62 @@
+package com.yechaoa.yutils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by yechao on 2017/3/21.
+ * Describe : 直接解析Json
+ * <p>
+ * GitHub : https://github.com/yechaoa
+ * CSDN : http://blog.csdn.net/yechaoa
+ */
+
+public class ParseUtil {
+
+    /**
+     * {
+     * "code": "0",
+     * "data": "修改成功",
+     * "flag": true,
+     * "info": null,
+     * "infoValues": null
+     * }
+     */
+
+    //解析Code
+    public static String parseCode(String response) {
+        String code = "";
+        try {
+            JSONObject json = new JSONObject(response);
+            code = json.getString("code");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return code;
+    }
+
+    //解析Flag
+    public static boolean parseFlag(String response) {
+        boolean flag = false;
+        try {
+            JSONObject json = new JSONObject(response);
+            flag = json.getBoolean("flag");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    //解析data
+    public static String parseData(String response, String key) {
+        String data = "";
+        try {
+            JSONObject json = new JSONObject(response);
+            data = json.getString(key);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+}
